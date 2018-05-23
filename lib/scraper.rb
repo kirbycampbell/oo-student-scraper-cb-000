ba6f1f1a3a1e90e4a04f524ae9092a0f7f71e79a
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-23 08:31:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,49 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    webpage = Nokogiri::HTML(html)
+    persons = []
+
+    webpage.css(".student-card").each do |person|
+      name = person.css("h4").text
+      location = person.css("p").text
+      profile_url = person.css("a").attribute("href").value
+
+        persons.push(
+        name: name,
+        location: location,
+        profile_url: profile_url
+      )
+    end
+    persons
   end
 
   def self.scrape_profile_page(profile_url)
-    
+      info = {}
+      html = open(profile_url)
+      profile = Nokogiri::HTML(html)
+
+      profile.css(".social-icon-container a").each do |social|
+        if social.attribute("href").value.include?("twitter")
+          info[:twitter] = social.attribute("href").value
+        elsif social.attribute("href").value.include?("linkedin")
+          info[:linkedin] = social.attribute("href").value
+        elsif social.attribute("href").value.include?("github")
+          info[:github] = social.attribute("href").value
+        else
+          info[:blog] = social.attribute("href").value
+        end
+      end
+      info[:profile_quote] = profile.css(".profile-quote").text
+      info[:bio] = profile.css("p").text
+
+      info
   end
 
-end
 
+end
